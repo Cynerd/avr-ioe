06ba0485fbcafca136fdc48285262b26a729a972
Karel Kočí <cynerd@email.cz>
2015-03-22 14:07:21 +0100

    Prepare for usb_cdc

---
+++ src/usb_cdc.c
@@ -0,0 +1,4 @@
+#include "../usb_cdc.h"
+
+void ioe_usb_cdc_init(void) {
+}
---
+++ usb_cdc.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <avr/io.h>
+
+#ifndef _IOE_USB_CDC_H_
+#define _IOE_USB_CDC_H_
+
+void ioe_usb_cdc_init(void);
+
+
+#endif /* _IOE_USB_CDC_H_ */
