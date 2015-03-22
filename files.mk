f5eafa85c5eba45d09b908a45671cd84e4aeb07d
Karel Kočí <cynerd@email.cz>
2015-03-22 14:05:39 +0100

    Prepare for DHT22

--- files.mk
+++ files.mk
@@ -2,6 +2,7 @@ ifndef IOE_PREFIX
 	IOE_PREFIX = .
 endif
 
+include dht22/files.mk
 IOE_SPI_SRC = $(IOE_PREFIX)/src/spi.c
 IOE_SPI_USI_SRC = $(IOE_PREFIX)/src/spi_usi.c
 IOE_SPI_USART_SRC = $(IOE_PREFIX)/src/spi_usart.c
