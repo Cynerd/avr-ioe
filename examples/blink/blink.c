5ab2332addae2a90a1fa438c91e8069ebbfc4082
Karel Kočí <cynerd@email.cz>
2017-01-30 20:45:41 +0100

    Cleanup repository
    I don't have enough time to implement everything so I am cutting
    losses. I might return to it later on, but now I push forward to
    implement at least base features.

--- examples/blink/blink.c
+++ examples/blink/blink.c
@@ -3,12 +3,12 @@
 #include <ioport.h>
 
 int main() {
-    io_setout(IO_B0);
+	io_setout(IO_B0);
 
-    while (1) {
-        io_hight(IO_B0);
-        _delay_ms(500);
-        io_low(IO_B0);
-        _delay_ms(500);
-    }
+	while (1) {
+		io_hight(IO_B0);
+		_delay_ms(500);
+		io_low(IO_B0);
+		_delay_ms(500);
+	}
 }
--- include/jobs.h
+++
@@ -1,8 +0,0 @@
-#include <stdint.h>
-
-#ifndef _IOE_JOBS_H_
-#define _IOE_JOBS_H_
-
-
-
-#endif /* _IOE_JOBS_H_ */
